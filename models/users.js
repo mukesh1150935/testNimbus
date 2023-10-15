ccc243e2356fac9ac6379617afce460ed5583c6d
mukesh <mukesh1150935@gmail.com>
2023-10-15 22:25:12 +0530

    controllers and model user updated

--- models/users.js
+++ models/users.js
@@ -1,82 +1,71 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
-    {
-        firebaseId: {
-            type: String,
-            required: true,
-            
-        },
-        userName: {
-            type: String,
-            minLength: 1,
-            maxLength: 128,
-            required: true,
-            
-        },
-        phoneNumber: {
-            type: String,
-            minLength: 10,
-            maxLength: 10,
-            
-        
-        },
-        password: {
-            type: String,
-            
-        },
-        email: {
-            type: String,
-            
-        },
-        fullName: {
-            type: String,
-            minLength: 1,
-            maxLength: 255,
-            
-        },
-        omegeleReports: {
-            type: Number,
-            default: 0,
-        },
-        OmegleAllowed: {
-            type: Boolean,
-            default: true,
-        },
-        favTeamVote: {
-            type: Boolean,
-            default: false,
-        },
-        instaId: {
-            type: String,
-            
-            default: "",
-            maxLength: 250,
-        },
-        profileImage: {
-            type: String,
-            maxLength: 255,
-            default: "",
-            
-        },
-        totalScore: {
-            type: Number,
-            default: 0,
-        },
-        linkedIn: {
-            type: String,
-            default: "",
-            maxLength: 255,
-            
-        },
-        github: {
-            type: String,
-            default: "",
-            maxLength: 255,
-            
-        },
+  {
+    firebaseId: {
+      type: String,
+      required: true,
     },
-    { timestamps: true }
+    userName: {
+      type: String,
+      minLength: 1,
+      maxLength: 128,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      unique:false
+    },
+    password: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required:true
+    },
+    fullName: {
+      type: String,
+      minLength: 1,
+      maxLength: 255,
+    },
+    omegeleReports: {
+      type: Number,
+      default: 0,
+    },
+    OmegleAllowed: {
+      type: Boolean,
+      default: true,
+    },
+    favTeamVote: {
+      type: Boolean,
+      default: false,
+    },
+    instaId: {
+      type: String,
+      default: "",
+      maxLength: 250,
+    },
+    profileImage: {
+      type: String,
+      maxLength: 255,
+      default: "",
+    },
+    totalScore: {
+      type: Number,
+      default: 0,
+    },
+    linkedIn: {
+      type: String,
+      default: "",
+      maxLength: 255,
+    },
+    github: {
+      type: String,
+      default: "",
+      maxLength: 255,
+    },
+  },
+  { timestamps: true }
 );
 
-export const User = mongoose.model("User", userSchema);
+export const User = mongoose.model("User1", userSchema);
